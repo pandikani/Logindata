6c14c7682376909db66ccd0c6530402ce8487cb4
pandikani <pandikani2101@gmail.com>
2025-07-22 10:45:42 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('https://loginserver-d9uf.onrender.com/api/login/', formData)
+      await axios.post('https://loginserver-d9uf.onrender.com/api/login/', formData,
+        {
+        withCredentials: true
+      })
       alert("Login info sent to Django!");
     } catch (error) {
       console.error('Error:', error);
